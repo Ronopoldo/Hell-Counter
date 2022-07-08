a635c4ea51564dd6e45a1cf61e5f5d22c5bb78d4
Ronopoldo <Ronopoldo@yandex.ru>
2022-07-08 23:38:46 +0300

    workin' answer

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-//Начало)))00 8.07.2022 11:14:20 pm
+const TelegramBot = require('node-telegram-bot-api')
+
+const token = '5468851871:AAE9lBfufLXb7LnjeQAIR6b3eYTXc1fqiOo'
+
+const bot = new TelegramBot(token, { polling: true })
+
+
+console.log('READY')
+
+
+
+
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+	const chatId = msg.chat.id
+	const resp = match[1]
+	bot.sendMessage(chatId, resp)
+})
+
